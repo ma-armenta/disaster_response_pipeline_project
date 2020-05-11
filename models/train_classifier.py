3850f07463089955c63bae3fca43caf12b8bb3e1
Armenta, Miguel (ma826r) <ma826r@us.att.com>
2020-05-10 21:11:38 -0500

    Pushing code before adding comments to code to meet project requirements....................

---
+++ models/train_classifier.py
@@ -0,0 +1,114 @@
+import sys
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import nltk
+nltk.download(['punkt', 'wordnet'])
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+import re
+from sklearn.svm import SVC
+import pickle
+
+def load_data(database_filepath):
+    # load data
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql("select * from Clean_Messages", engine)
+
+    # separate datasets
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis = 1)
+
+    # get category names
+    category_names = Y.columns.values
+
+    return X, Y, category_names
+
+def tokenize(text):
+    text = text.lower()
+    # Remove punctuation characters
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators = 10,
+                                                             min_samples_split = 10)))
+    ])
+    
+    # For some reason adding parameters never worked for me. I tried this on the jupyter notebook
+    # as well as on my computer.
+    # This issue happens whenever I used MultiOutputClassifier as directed by the project instructions.
+    # add parameters for grid search
+    # parameters = {'vect__min_df': [1, 5],
+    #               'tfidf__use_idf':[True, False],
+    #               'clf__estimator__n_estimators':[10, 25], 
+    #               'clf__estimator__min_samples_split':[2, 5, 10]}
+    # cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    # return cv
+    return pipeline
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    for i in range(len(category_names)):
+        print(category_names[i])
+        print(classification_report(Y_test.iloc[:,i],y_pred[:,i]))
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
